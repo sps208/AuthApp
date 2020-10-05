017f9a4d3c0f0d18feb2da0053877caa9fc19052
sps208 <satriops20@gmail.com>
2020-10-05 16:27:52 +0700

    checkpoint 5

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
-import {TextInput, View, Text } from 'react-native';
+import {TextInput, View, Text, StyleSheet } from 'react-native';
+
+const style = StyleSheet.create({
+    label: {
+        fontSize: 18,
+        paddingLeft:20,
+        flex: 1
+    },
+    input: {
+        color: "black",
+        paddingHorizontal: 5,
+        fontSize: 18,
+        lineHeight: 23,
+        height: 50,
+        width: 100,
+        flex: 2
+    },
+    container: {
+        height: 50,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        borderBottomColor: '#007aff',
+        borderBottomWidth: 1
+    }
+})
 
 class Input extends Component {
     render(){
         const { label, placeholder, secureTextEntry }= this.props
         return(
-            <View>
-                <Text>{ label }</Text>
+            <View style={ style.container }>
+                <Text style={ style.label }>{ label }</Text>
                 <TextInput 
+                    style={ style.input }
                     placeholder={ placeholder }
                     secureTextEntry={ secureTextEntry }
                     autoCorrect={ false } />
