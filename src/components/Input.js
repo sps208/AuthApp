47e2241e1f1df214129f2cb219a6d2bd27697c8d
sps208 <satriops20@gmail.com>
2020-10-05 16:15:49 +0700

    checkpoint 4

---
+++ src/components/Input.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {TextInput, View, Text } from 'react-native';
+
+class Input extends Component {
+    render(){
+        const { label, placeholder, secureTextEntry }= this.props
+        return(
+            <View>
+                <Text>{ label }</Text>
+                <TextInput 
+                    placeholder={ placeholder }
+                    secureTextEntry={ secureTextEntry }
+                    autoCorrect={ false } />
+            </View>
+        );
+    }
+}
+
+export default Input;
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,13 +3,18 @@ import React, { Component } from 'react';
 import Card from './Card'
 import CardSection from './CardSection'
 import Button from './Button'
+import Input from './Input'
 
 class LoginForm extends Component {
     render(){
         return(
             <Card>
-                <CardSection></CardSection>
-                <CardSection></CardSection>
+                <CardSection>
+                    <Input label="Email" placeholder="email1@gmail.com" />
+                </CardSection>
+                <CardSection>
+                <Input label="Password" placeholder="password" />
+                </CardSection>
                 <CardSection>
                     <Button>
                         Login
